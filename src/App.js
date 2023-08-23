53d6c14e6d67313d7aa6cb4b87ef1c624174a24e
Filipejoaoss <seica.filipejoao@gmail.com>
2023-08-23 12:26:42 +0100

    StopWatch Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Component} from "react";
+import "./sytle.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: 0,
+            btn: 'Start'
+        }
+
+        this.time = null;
+        this.start = this.start.bind(this);
+        this.clear = this.clear.bind(this);
+    }
+
+    start() {
+        let state = this.state;
+
+        if (this.time !== null) {
+            clearInterval(this.time);
+
+            this.time = null;
+
+            state.btn = 'Start';
+        } else {
+            this.time = setInterval(() => {
+                let state = this.state;
+
+                state.number += 0.1;
+
+                this.setState(state);
+
+                state.btn = 'Stop'
+            }, 100);
+        }
+
+        this.setState(state);
+    }
+
+    clear() {
+        let state = this.state;
+
+        if (this.time !== null) {
+            clearInterval(this.time);
+
+            this.time = null;
+        }
+
+        state.number = 0;
+        state.btn = 'Start';
+        this.setState(state);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <img src={require('./assets/cronometro.png')} alt="cronometro" className="img" />
+
+                <a className="time"> { this.state.number.toFixed(1) } </a>
+
+                <div className="btnArea">
+                    <a className="btn" onClick={ this.start } > { this.state.btn } </a>
+
+                    <a className="btn" onClick={ this.clear } > Clear </a>
+                </div>
+            </div>
+        );
+
+    }
 }
 
 export default App;
